f2d92b1c6c9203cb8f65566e014f3208b9d18462
neusfer <125355473+neusfer@users.noreply.github.com>
2023-03-07 10:11:09 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
 let Start = 0
 input.onLogoEvent(TouchButtonEvent.Touched, function () {
     Start = input.runningTime()
-    basic.showIcon(IconNames.Heart)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # # . # #
+        . # # # .
+        . . # . .
+        `)
     basic.showNumber(0)
 })
 basic.forever(function () {
