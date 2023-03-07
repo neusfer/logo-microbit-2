e8531b29cffb287e8e3a35c05512f43d48795984
neusfer <125355473+neusfer@users.noreply.github.com>
2023-03-07 10:37:22 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,19 @@
 let Start = 0
+let Time = 0
 input.onLogoEvent(TouchButtonEvent.Touched, function () {
     Start = input.runningTime()
     basic.showLeds(`
-        . . # . .
-        . # # # .
-        # # . # #
-        . # # # .
-        . . # . .
+        # . . . #
+        # # . . #
+        # . # . #
+        # . . # #
+        # . . . #
         `)
-    basic.showNumber(0)
 })
-basic.forever(function () {
-	
+input.onLogoEvent(TouchButtonEvent.Released, function () {
+    Time = input.runningTime() - Start
+    basic.showNumber(Math.idiv(Time, 1000))
+    basic.pause(100)
+    basic.clearScreen()
+    basic.showNumber(Time % 1000)
 })
